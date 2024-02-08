ddae76b77bcb7b560bf752bb01d8e195024860d8
ASSIMv3 <hassanassim01@gmail.com>
2024-02-08 14:19:37 +0100

    FIXING TASK 0

--- 0x03-React_component/task_0/dashboard/src/index.js
+++ 0x03-React_component/task_0/dashboard/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import * as ReactDOM from 'react-dom'
 import App from "./App/App";
 
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
