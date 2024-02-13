5c7d85125bd10afce951140ad01509a228d09b9c
ASSIMv3 <hassanassim01@gmail.com>
2024-02-13 13:54:09 +0100

    fixing task 0

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { shallow } from "enzyme";
 import CourseListRow from "./CourseListRow";
+import { StyleSheetTestUtils } from 'aphrodite';
 
 describe("Testing <CourseListRow />", () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
   it("When isHeader is true test the component renders one cell with colspan = 2 when textSecondCell does not exist", () => {
     let wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="first cell test"/>);
     expect(wrapper.find('th')).toHaveLength(1);
     expect(wrapper.find('th').prop('colSpan')).toEqual('2');
     expect(wrapper.find('th').text()).toEqual('first cell test');
-    expect(wrapper.find('th').prop('style')).toHaveProperty('backgroundColor', '#deb5b545');
   });
 
   it("When isHeader is true test the component renders two cells when textSecondCell is present", () => {
@@ -16,7 +20,6 @@ describe("Testing <CourseListRow />", () => {
     expect(wrapper.find('th')).toHaveLength(2);
     expect(wrapper.find('th').at(0).text()).toEqual('first cell test');
     expect(wrapper.find('th').at(1).text()).toEqual('second cell test');
-    expect(wrapper.find('th').everyWhere(node => node.prop('style'))).toHaveProperty('backgroundColor', '#deb5b545');
   });
 
   it("When isHeader is false test the component renders correctly two td elements within a tr element", () => {
@@ -24,6 +27,5 @@ describe("Testing <CourseListRow />", () => {
     expect(wrapper.find('td')).toHaveLength(2);
     expect(wrapper.find('td').at(0).text()).toEqual('first cell test');
     expect(wrapper.find('td').at(1).text()).toEqual('second cell test');
-    expect(wrapper.find('tr').prop('style')).toHaveProperty('backgroundColor', '#f5f5f5ab');
   });
 });
