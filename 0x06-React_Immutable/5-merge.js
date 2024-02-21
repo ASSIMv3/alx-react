2fd4df5db9a45b0c4508d008d3a577273517e053
ASSIMv3 <hassanassim01@gmail.com>
2024-02-21 11:15:39 +0100

    task 5

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,4 +1,15 @@
-import { List, Map } from 'immutable';
+import { Map, List } from 'immutable';
 
-export const conactElements = (page1, page2) => List(page).concat(List(page2));
-export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
+export function concatElements(page1, page2) {
+  const list1 = List(page1);
+  const list2 = List(page2);
+
+  return list1.concat(list2);
+}
+
+export function mergeElements(page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+
+  return map1.merge(map2);
+}
