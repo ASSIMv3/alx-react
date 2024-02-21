610b6ed04edfd83e05900ba801e374d255075ea0
ASSIMv3 <hassanassim01@gmail.com>
2024-02-21 11:18:55 +0100

    task 8

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,20 +1,24 @@
-import { Seq, Map } from 'immutable';
+import { Seq } from 'immutable';
 
 export default function printBestStudents(object) {
-  const filtered = Seq(object)
-    .filter((student) => Map(student).get('score') > 70)
-    .map((student) =>
-      Map(student)
-        .update(
-          'firstName',
-          (firstName) => firstName.charAt(0).toUpperCase() + firstName.slice(1)
-        )
-        .update(
-          'lastName',
-          (lastName) => lastName.charAt(0).toUpperCase() + lastName.slice(1)
-        )
-    )
-    .toJS();
+  const seq = Seq(object);
 
-  console.log(filtered);
+  const filtered = seq.filter((student) => {
+    student.firstName.charAt(0).toUpperCase();
+    return student.score > 70;
+  });
+
+  function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const JSObject = filtered.toJS();
+
+  Object.keys(JSObject).map((key) => {
+    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
+    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
+    return JSObject[key];
+  });
+
+  console.log(JSObject);
 }
