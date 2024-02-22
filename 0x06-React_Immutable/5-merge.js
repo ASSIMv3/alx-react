b5e28d7e62d6c31ca79c9eb1fdd4641d5195fe45
ASSIMv3 <113550604+ASSIMv3@users.noreply.github.com>
2024-02-22 15:24:58 +0100

    Update 5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,13 +1,13 @@
 import { Map, List } from 'immutable';
 
-export function concatElements (page1, page2) {
+export function concatElements(page1, page2) {
   const list1 = List(page1);
   const list2 = List(page2);
 
   return list1.concat(list2);
 }
 
-export function mergeElements (page1, page2) {
+export function mergeElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
 
