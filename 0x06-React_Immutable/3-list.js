b2dc48b9e6e200c22b7a672f21c2516d61eea8d0
ASSIMv3 <113550604+ASSIMv3@users.noreply.github.com>
2024-02-22 14:19:20 +0100

    Update 3-list.js

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,9 +1,9 @@
-import { List } from 'immutable';
+import { List } from './node_modules/immutable/dist/immutable';
 
 export function getListObject(array) {
-  return List(array);
+    return List(array);
 }
 
 export function addElementToList(list, element) {
-  return list.push(element);
+    return list.push(element);
 }
