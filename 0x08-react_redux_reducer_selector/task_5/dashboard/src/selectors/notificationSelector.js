b4b0c2f908f17e58fb80170a22dd87a16c11fbb7
ASSIMv3 <hassanassim01@gmail.com>
2024-03-04 11:30:49 +0100

    MANDATORY TASK 5

--- 0x08-react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.js
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.js
@@ -1,1 +1,6 @@
+export const filterTypeSelected = state => state.get('filter');
 
+export const getNotifications = state => state.get('notifications');
+
+export const getUnreadNotifications = state =>
+  state.get('notifications').filter(notification => !notification.get('isRead'));
--- 0x08-react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.test.js
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.test.js
@@ -1,1 +1,35 @@
+import { fromJS } from 'immutable';
+import {
+  filterTypeSelected,
+  getNotifications,
+  getUnreadNotifications,
+} from './notificationSelector';
 
+describe('Notification Selectors', () => {
+  const initialState = fromJS({
+    filter: 'DEFAULT',
+    notifications: [
+      { id: 1, isRead: false, type: 'default', value: 'New course available' },
+      { id: 2, isRead: false, type: 'urgent', value: 'New resume available' },
+      { id: 3, isRead: true, type: 'urgent', value: 'Read data available' },
+    ],
+  });
+
+  it('filterTypeSelected should return the filter value', () => {
+    const result = filterTypeSelected(initialState);
+    expect(result).toEqual('DEFAULT');
+  });
+
+  it('getNotifications should return the list of notifications', () => {
+    const result = getNotifications(initialState);
+    expect(result.toJS()).toEqual(initialState.get('notifications').toJS());
+  });
+
+  it('getUnreadNotifications should return the list of unread notifications', () => {
+    const result = getUnreadNotifications(initialState);
+    expect(result.toJS()).toEqual([
+      { id: 1, isRead: false, type: 'default', value: 'New course available' },
+      { id: 2, isRead: false, type: 'urgent', value: 'New resume available' },
+    ]);
+  });
+});
